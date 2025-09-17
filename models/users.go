b5f0999aa9a90c2abb02dc077937812a8a12f5b6
Labib Abdullah <labibabdullahhasan@gmail.com>
2025-09-17 21:45:45 +0700

    created models

---
+++ models/users.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	InternalID int64          `json:"internal_id" db:"internal_id" gorm:"PrimaryKey"`
+	PublicID   uuid.UUID      `json:"public_id" db:"public_id"`
+	Name       string         `json:"name" db:"name"`
+	Email      string         `json:"email" db:"email"`
+	Password   string         `json:"password" db:"password" gorm:"column:password"`
+	Role       string         `json:"role" db:"role"`
+	CreatedAt  time.Time      `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at" db:"updated-at"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"index"`
+}
